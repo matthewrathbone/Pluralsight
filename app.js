19335f17fd6a9654acbb81c460f5982e47ac9d4c
Matthew Rathbone <matthew.rathbone@sharpfutures.org.uk>
2016-09-08 09:11:46 +0100

    Setting up Gulp

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+  res.send('Hello World');
 });
 
 app.get('/books', function (req, res) {
-    res.send('Hello Books');
+  res.send('Hello Books');
 });
 
 app.listen(port, function (err) {
-    console.log('running server on port ' + port);
+  console.log('running server on port ' + port);
 });
